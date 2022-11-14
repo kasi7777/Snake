8ae672e67d3030478d01c656478344c670ef8949
kasi7777 <tatarin.ash@gmail.com>
2022-11-15 00:18:53 +0300

    2

--- ConsoleApp1/ConsoleApp1/Point.cs
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Snake
 {
     class Point
     {
@@ -23,10 +23,42 @@ namespace ConsoleApp1
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + " , " + y + " , " + sym;
         }
     }
 }
